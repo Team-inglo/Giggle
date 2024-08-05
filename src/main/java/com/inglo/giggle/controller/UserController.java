c8182a9d26bc1c704201689a4bed52edfef41fa4
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-08-05 12:46:39 +0900

    feat: 회원가입과 관련된 유저정보 API 구현

--- src/main/java/com/inglo/giggle/controller/UserController.java
+++ src/main/java/com/inglo/giggle/controller/UserController.java
@@ -2,6 +2,7 @@ package com.inglo.giggle.controller;
 
 import com.inglo.giggle.annotation.UserId;
 import com.inglo.giggle.dto.global.ResponseDto;
+import com.inglo.giggle.dto.request.updateUserDto;
 import com.inglo.giggle.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,16 +26,64 @@ public class UserController {
         return ResponseDto.ok(userService.readUser(userId));
     }
 
-    @PatchMapping(value = "", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    @Operation(summary = "본인 정보 수정(스펙 등록)", description = "자격증을 등록하여 본인 정보를 업데이트합니다..")
-    public ResponseDto<?> updateUser(
+    @PostMapping(value = "/passport", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(summary = "여권 등록", description = "여권 정보를 등록합니다.")
+    public ResponseDto<?> updatePassport(
             @UserId Long userId,
-            @RequestParam(required = false) String nickname,
-            @RequestPart(value = "file", required = false)
-            MultipartFile imgFile) {
-        // ToDo: OCR 사용하여 자격증 파일 읽어와, 자격증 정보를 등록하고, 유저 정보 업데이트
-        userService.updateUser(userId);
+            @RequestPart(value = "file", required = true) MultipartFile file
+    ) {
+        userService.updatePassport(userId, file);
         return ResponseDto.ok(null);
     }
 
+    @PostMapping(value = "/registration", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(summary = "외국인 등록증 등록", description = "외국인 등록증을 등록합니다.")
+    public ResponseDto<?> updateRegistration(
+            @UserId Long userId,
+            @RequestPart(value = "file", required = true) MultipartFile file
+    ) {
+        userService.updateRegistration(userId, file);
+        return ResponseDto.ok(null);
+    }
+
+    @PatchMapping("")
+    @Operation(summary = "거주지 수정", description = "본인 정보 중 유일하게 수정 가능한 거주지를 업데이트합니다.")
+    public ResponseDto<?> updateAddress(
+            @UserId Long userId,
+            @RequestBody updateUserDto updateUserDto
+    ) {
+        userService.updateAddress(userId, updateUserDto.address());
+        return ResponseDto.ok(null);
+    }
+
+    @PostMapping(value = "/topik", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "스펙 등록(토픽)", description = "토픽 자격증을 등록하여 본인 정보를 업데이트합니다.")
+    public ResponseDto<?> updateTopik(
+            @UserId Long userId,
+            @RequestPart(value = "file", required = false)
+            MultipartFile file) {
+        // ToDo: OCR 사용하여 자격증 파일 읽어와, 자격증 정보를 등록하고, 유저 정보 업데이트
+        userService.updateTopik(userId, file);
+        return ResponseDto.ok(null);
+    }
+
+    @PostMapping(value = "/social-integration-program", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "스펙 등록(사회통합프로그램)", description = "사회통합프로그램 자격증을 등록하여 본인 정보를 업데이트합니다.")
+    public ResponseDto<?> updateSocialIntegrationProgram(
+            @UserId Long userId,
+            @RequestPart(value = "file", required = false)
+            MultipartFile file) {
+        userService.updateSocialIntegrationProgram(userId, file);
+        return ResponseDto.ok(null);
+    }
+
+    @PostMapping(value = "/sejong-institute", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "스펙 등록(세종학당)", description = "세종학당 자격증을 등록하여 본인 정보를 업데이트합니다.")
+    public ResponseDto<?> updateSejongInstitute(
+            @UserId Long userId,
+            @RequestPart(value = "file", required = false)
+            MultipartFile file) {
+        userService.updateSejongInstitute(userId, file);
+        return ResponseDto.ok(null);
+    }
 }
--- src/main/java/com/inglo/giggle/exception/CommonException.java
+++ src/main/java/com/inglo/giggle/exception/CommonException.java
@@ -4,12 +4,24 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CommonException extends RuntimeException {
     private final ErrorCode errorCode;
+    private final String detailedMessage;
+
+    public CommonException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.detailedMessage = message;
+    }
+
+    public CommonException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.detailedMessage = null;
+    }
 
     @Override
     public String getMessage() {
-        return errorCode.getMessage();
+        return detailedMessage != null ? detailedMessage : errorCode.getMessage();
     }
 }
