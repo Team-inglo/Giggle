6f6b9f6d73826d192e92789108d1c0f72b65aef9
장동겸 <jangelliot0404@dgu.ac.kr>
2024-08-05 15:58:43 +0900

    🚑 fix: 유저 정보 수정에 gpa, startDay, endDay 추가 (#7)

--- src/main/java/com/inglo/giggle/dto/request/updateUserDto.java
+++ src/main/java/com/inglo/giggle/dto/request/updateUserDto.java
@@ -1,9 +1,20 @@
 package com.inglo.giggle.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.inglo.giggle.annotation.Date;
 
-public record updateUserDto(
+import java.time.LocalDateTime;
+
+public record UpdateUserDto(
         @JsonProperty("address")
-        String address
+        String address,
+        @JsonProperty("gpa")
+        String gpa,
+        @JsonProperty("startDay")
+        @Date
+        LocalDateTime startDay,
+        @JsonProperty("endDay")
+        @Date
+        LocalDateTime endDay
 ) {
 }
