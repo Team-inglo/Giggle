64f1b2fde1efd1c5ec1887f7e6afe6c6f29238fb
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-08-14 17:40:07 +0900

    feat: partTime 생성, 조회 api 구현

--- src/main/java/com/inglo/giggle/controller/PartTimeController.java
+++ src/main/java/com/inglo/giggle/controller/PartTimeController.java
@@ -1,15 +1,33 @@
 package com.inglo.giggle.controller;
 
+import com.inglo.giggle.annotation.UserId;
+import com.inglo.giggle.dto.global.ResponseDto;
+import com.inglo.giggle.dto.request.PartTimeCreateDto;
 import com.inglo.giggle.service.PartTimeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "아르바이트 기록", description = "사용자의 아르바이트 기록 관련 API")
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/part-time")
 public class PartTimeController {
     private final PartTimeService partTimeService;
+
+    @PostMapping("")
+    public ResponseDto<?> createPartTime(
+            @UserId Long userId,
+            @RequestBody PartTimeCreateDto partTimeCreateDto
+    ) {
+        partTimeService.createPartTime(userId, partTimeCreateDto);
+        return ResponseDto.created(null);
+    }
+
+    @GetMapping("/{partTimeId}")
+    public ResponseDto<?> getPartTime(
+            @PathVariable Long partTimeId
+    ) {
+        return ResponseDto.ok(partTimeService.getPartTime(partTimeId));
+    }
 }
--- src/main/java/com/inglo/giggle/service/PartTimeService.java
+++ src/main/java/com/inglo/giggle/service/PartTimeService.java
@@ -1,11 +1,43 @@
 package com.inglo.giggle.service;
 
+import com.inglo.giggle.domain.PartTime;
+import com.inglo.giggle.domain.User;
+import com.inglo.giggle.dto.request.PartTimeCreateDto;
+import com.inglo.giggle.dto.response.PartTimeDto;
+import com.inglo.giggle.exception.CommonException;
+import com.inglo.giggle.exception.ErrorCode;
 import com.inglo.giggle.repository.PartTimeRepository;
+import com.inglo.giggle.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PartTimeService {
     private final PartTimeRepository partTimeRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void createPartTime(Long userId, PartTimeCreateDto partTimeCreateDto) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
+        try {
+            partTimeRepository.save(
+                    PartTime.builder()
+                            .partTimeName(partTimeCreateDto.name())
+                            .hourlyRate(partTimeCreateDto.hourlyRate())
+                            .color(partTimeCreateDto.color())
+                            .user(user)
+                            .build()
+            );
+        } catch(Exception e) {
+            throw new CommonException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public PartTimeDto getPartTime(Long partTimeId) {
+        PartTime partTime = partTimeRepository.findById(partTimeId).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_RESOURCE));
+
+        return PartTimeDto.fromEntity(partTime);
+    }
 }
