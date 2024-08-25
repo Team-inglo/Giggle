86222885f54dbaf1afb4780831f016efe12b00ac
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-08-26 02:14:06 +0900

    fix: endpoint id-duplicate으로 변경

--- src/main/java/com/inglo/giggle/constants/Constants.java
+++ src/main/java/com/inglo/giggle/constants/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     public static List<String> NO_NEED_AUTH_URLS = List.of(
             "/api/v1/no-auth/**", "api/v1/oauth/login",
             "/api/v1/auth/sign-up",
-            "/api/v1/auth/email-duplicate",
+            "/api/v1/auth/id-duplicate",
             "/api-docs.html",
             "/api-docs/**",
             "/swagger-resources/**",
