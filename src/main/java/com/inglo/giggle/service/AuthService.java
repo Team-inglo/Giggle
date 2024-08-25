f3babb35537a4f2d13a4c8a78e4ed113867296e3
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-08-26 02:15:55 +0900

    feat: 아이디 중복 검사 시 더미 삭제 로직 추가 / 회원가입 후 토큰 반환 로직 추가

--- src/main/java/com/inglo/giggle/service/AuthService.java
+++ src/main/java/com/inglo/giggle/service/AuthService.java
@@ -29,27 +29,55 @@ public class AuthService {
     private final ApplicantFileRepository applicantFileRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public boolean checkDuplicate(String email) {
-        return userRepository.existsBySerialId(email);
+    public boolean checkDuplicate(String serialId) { // 아이디 중복 검사. 단, 아이디가 존재하더라도 그 계정의 데이터가 비어있다면 삭제하고 중복이 아닌 것으로 간주
+        User user = userRepository.findBySerialId(serialId).orElse(null);
+        // 중복 안되는 경우
+        if (user == null) return true;
+        // 중복인데 그 계정의 데이터가 비어있는 경우
+        else {
+            if (user.getRole() == ERole.APPLICANT) {
+                Applicant applicant = applicantRepository.findByUser(user).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_APPLICANT));
+                // 유학생이고 회원가입을 끝까지 안한경우
+                if (applicant.getRegistrationNumber() == null) {
+                    applicantFileRepository.deleteByApplicant(applicant);
+                    applicantRepository.delete(applicant);
+                    userRepository.delete(user);
+                    return true;
+                }
+            }
+            // 점주고 회원가입을 끝까지 안한경우
+            if (user.getRole() == ERole.OWNER) {
+                Owner owner = ownerRepository.findByUser(user).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_OWNER));
+                if (owner.getStoreName() == null) {
+                    ownerRepository.delete(owner);
+                    userRepository.delete(user);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Transactional
-    public void signUp(AuthSignUpDto authSignUpDto) { // 아이디 비밀번호 등록 시, User와 UserFile을 생성
+    public JwtTokenDto signUp(AuthSignUpDto authSignUpDto) { // 아이디 비밀번호 등록 시, User와 UserFile을 생성
         ERole role = ERole.fromName(authSignUpDto.role());
-
+        User user;
         if (role == ERole.APPLICANT) {
-            User user = userRepository.save(
+            user = userRepository.save(
                     User.signUp(authSignUpDto, bCryptPasswordEncoder.encode(authSignUpDto.password()), role)
             );
             Applicant applicant = applicantRepository.save(Applicant.signUp(user));
             applicantFileRepository.save(ApplicantFile.signUp(applicant));
         }
         else {
-            User user = userRepository.save(
+            user = userRepository.save(
                     User.signUp(authSignUpDto, bCryptPasswordEncoder.encode(authSignUpDto.password()), role)
             );
             ownerRepository.save(Owner.signUp(user));
         }
+        JwtTokenDto jwtTokenDto = jwtUtil.generateTokens(user.getId(), user.getRole());
+        user.updateRefreshToken(jwtTokenDto.refreshToken());
+        return jwtTokenDto;
     }
 
     @Transactional
