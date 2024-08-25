56bc700b518db37efcdcee5620ff6bb370765073
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-08-26 07:16:52 +0900

    feat: Operation 추가

--- src/main/java/com/inglo/giggle/controller/OwnerController.java
+++ src/main/java/com/inglo/giggle/controller/OwnerController.java
@@ -4,6 +4,7 @@ import com.inglo.giggle.annotation.UserId;
 import com.inglo.giggle.dto.global.ResponseDto;
 import com.inglo.giggle.dto.request.UpdateOwnerDto;
 import com.inglo.giggle.service.OwnerService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ public class OwnerController {
     private final OwnerService ownerService;
 
     @PatchMapping("")
+    @Operation(summary = "owner 상세정보 수정", description = "owner 상세정보를 수정합니다.")
     public ResponseDto<?> updateOwner(
             @UserId Long userId,
             @RequestBody UpdateOwnerDto updateOwnerDto
